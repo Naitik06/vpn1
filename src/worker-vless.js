957ec4ac30866a256f876c8ed0d9654acd4d43ba
Naitik06 <93211114+Naitik06@users.noreply.github.com>
2026-03-24 16:05:47 +0530

    Update worker-vless.js

--- src/worker-vless.js
+++ src/worker-vless.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = 'de6c75f5-de56-4991-b7e6-efa9f3d1f94b';
 
 let proxyIP = '';
 
